dbad94890adf4cf793b229f3a2c17153667fdc30
Salman964 <salmansaeed964@gmail.com>
2024-11-29 23:34:26 +0500

    BayzDeliveryApplication - initial Commit

---
+++ src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
@@ -0,0 +1,13 @@
+// package com.bayzdelivery;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.scheduling.annotation.EnableScheduling;
+
+// @SpringBootApplication
+public class BayzDeliveryApplication {
+  public static void main(String[] args) {
+    System.out.println("Testing");
+    // SpringApplication.run(BayzDeliveryApplication.class, args);
+  }
+}
