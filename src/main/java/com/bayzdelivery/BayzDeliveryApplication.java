dd0d9e9daef1f82764247af2afd9da7b09889b4d
Salman964 <salmansaeed964@gmail.com>
2024-12-01 17:42:42 +0500

    feat: enable scheduling to notify support for delayed deliveries

--- src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
+++ src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-// @EnableScheduling
 public class BayzDeliveryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BayzDeliveryApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BayzDeliveryApplication.class, args);
+  }
 }
